1291388219739ebaec8a134c273628f224b59612
Dima-Kutaini <deemaKutainy@gmail.com>
2023-12-12 13:58:42 +0100

    create form

---
+++ javascript.js
@@ -0,0 +1,6 @@
+
+ const form=document.querySelector("#create-account-form"); 
+ const usernameInput= document.querySelector("#username"); 
+ const emailInput=document.querySelector("#email"); 
+ const passwordInput=document.querySelector("#password"); 
+ const confirmPasswordInput=document.querySelector("#confirm-password"); 
